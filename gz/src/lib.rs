ecdf2bae0a0625a8a1207305e09554da27969e3c
Chen Weiguang <chen.weiguang@gmail.com>
2018-04-05 11:40:10 +0800

    Add unit tests

--- gz/src/lib.rs
+++ gz/src/lib.rs
@@ -38,3 +38,77 @@ impl Action for GzAction {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    extern crate tempfile;
+
+    use self::tempfile::NamedTempFile;
+    use super::*;
+    use flate2::{Compression, write::GzEncoder};
+    use std::io::{self, Write};
+
+    fn write_compressed_bytes<B>(bytes: B) -> io::Result<Vec<u8>>
+    where
+        B: AsRef<[u8]>,
+    {
+        let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+        encoder.write_all(bytes.as_ref())?;
+        encoder.finish()
+    }
+
+    fn write_tmpfile_bytes_wrapper<B>(bytes: B, compress: bool) -> ActionResult
+    where
+        B: AsRef<[u8]>,
+    {
+        let tmpfile = NamedTempFile::new();
+        assert!(tmpfile.is_ok());
+        let mut tmpfile = tmpfile.unwrap();
+
+        let bytes = bytes.as_ref();
+
+        let write_res = if compress {
+            let compressed_bytes = write_compressed_bytes(bytes);
+            assert!(compressed_bytes.is_ok());
+            let compressed_bytes = compressed_bytes.unwrap();
+
+            tmpfile.write_all(&compressed_bytes)
+        } else {
+            tmpfile.write_all(bytes)
+        };
+
+        assert!(write_res.is_ok());
+
+        let path = tmpfile.into_temp_path();
+        let exec_res = GzAction::execute(&path);
+
+        let close_res = path.close();
+        assert!(close_res.is_ok());
+
+        exec_res
+    }
+
+    #[test]
+    fn test_gz_action_execute_ok_1() {
+        let exec_res = write_tmpfile_bytes_wrapper(b"", true);
+        assert!(exec_res.is_ok());
+    }
+
+    #[test]
+    fn test_gz_action_execute_ok_2() {
+        let exec_res = write_tmpfile_bytes_wrapper(b"Hello World!", true);
+        assert!(exec_res.is_ok());
+    }
+
+    #[test]
+    fn test_gz_action_execute_fail_1() {
+        let exec_res = write_tmpfile_bytes_wrapper(b"", false);
+        assert!(exec_res.is_err());
+    }
+
+    #[test]
+    fn test_gz_action_execute_fail_2() {
+        let exec_res = write_tmpfile_bytes_wrapper(b"Hello World!", false);
+        assert!(exec_res.is_err());
+    }
+}
